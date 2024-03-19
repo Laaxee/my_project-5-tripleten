cad9609b99ed9f6bc4722c51179d9ed5ce754400
ALEX <alex.valen.2710@gmail.com>
2024-03-19 20:47:43 -0300

    Mensaje de confirmación

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# my_project-5-tripleten
-project 5
+#Proyecto de Exploración de Datos de Anuncios de Venta de Coches
+
+Este proyecto consiste en una aplicación web desarrollada con Streamlit que permite explorar datos de anuncios de venta de coches. La aplicación proporciona funcionalidades para visualizar y analizar los datos, incluyendo la construcción de histogramas y gráficos de dispersión.
+
+La aplicación web ofrece las siguientes funcionalidades:
+
+Exploración de Datos: Permite cargar y explorar datos de anuncios de venta de coches desde un archivo CSV.
+Construcción de Histogramas: Permite construir histogramas para visualizar la distribución de una variable específica, como el odómetro.
+Gráficos de Dispersión: Permite construir gráficos de dispersión para explorar la relación entre dos variables, como el odómetro y el precio, diferenciando por tipo de combustible.
+Esta aplicación es útil para analizar y comprender mejor los datos de anuncios de venta de coches, lo que puede ayudar en la toma de decisiones relacionadas con la compra, venta o análisis de mercado de vehículos usados.
--- app.py
+++ app.py
@@ -2,21 +2,46 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Leer los datos desde el archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
 
-# columna 'color' basada en el tipo de combustible
+# Crear una columna 'color' basada en el tipo de combustible
 car_data['color'] = car_data['fuel'].apply(lambda x: 'Gasolina' if x == 'gas' else 'Diésel')
 
-# gráfico de dispersión con Plotly Express
-fig = px.scatter(car_data, x="odometer", y="price", color='color',
-                 labels={'odometer': 'Odómetro (millas)', 'price': 'Precio (USD)', 'color': 'Tipo de Combustible'},
-                 title='Relación entre el Odómetro y el Precio de los Vehículos por Tipo de Combustible')
+# Crear un gráfico de dispersión con Plotly Express
+fig_scatter = px.scatter(car_data, x="odometer", y="price", color='color',
+                         labels={'odometer': 'Odómetro (millas)', 'price': 'Precio (USD)', 'color': 'Tipo de Combustible'},
+                         title='Relación entre el Odómetro y el Precio de los Vehículos por Tipo de Combustible')
 
-# Personalizar el aspecto del gráfico
-fig.update_traces(marker_size=8, opacity=0.7)
-
-# Añadir título a los ejes x e y
-fig.update_layout(xaxis_title='Odómetro (millas)', yaxis_title='Precio (USD)')
+# Personalizar el aspecto del gráfico de dispersión
+fig_scatter.update_traces(marker_size=8, opacity=0.7)
+fig_scatter.update_layout(xaxis_title='Odómetro (millas)', yaxis_title='Precio (USD)')
 
 # Mostrar el gráfico de dispersión en Streamlit
-st.plotly_chart(fig)
+st.plotly_chart(fig_scatter)
+
+# Encabezado
+st.header('Exploración de datos de anuncios de venta de coches')
+
+# Botón para construir un histograma
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    # Escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear un histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+    
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Botón para construir un gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    # Escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # Mostrar un gráfico de dispersión
+    st.plotly_chart(fig_scatter, use_container_width=True)
